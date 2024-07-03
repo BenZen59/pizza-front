7f5beb73bc366d3943e6cd6ee505646c6a69f04f
benoit <benoit.zenone@gmail.com>
2024-07-03 16:02:42 +0200

    add unite

--- src/app/components/ArticleCard/page.js
+++ src/app/components/ArticleCard/page.js
@@ -8,7 +8,6 @@ import {
   Dropdown,
   DropdownTrigger,
   DropdownMenu,
-  DropdownSection,
   DropdownItem,
 } from "@nextui-org/dropdown";
 import pizzaApi from "../../api/pizzaApi";
@@ -17,7 +16,7 @@ import Image from "next/image";
 export default function ArticleCard({ selectedCategorie }) {
   const [articles, setArticles] = useState([]);
   const [taille, setTaille] = useState(26); // Taille par défaut, peut être modifiée si nécessaire
-  console.log(selectedCategorie);
+  const [allArticles, setAllArticles] = useState([]); // Tous les articles non filtrés
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,12 +24,10 @@ export default function ArticleCard({ selectedCategorie }) {
         const response = await pizzaApi.getArticleWithTypeProduit(
           selectedCategorie
         );
-
-        // Vérifier si le type de produit est 'pizza'
         if (selectedCategorie === 1) {
-          // Si c'est une pizza, filtrer par taille
+          setAllArticles(response.data);
           const filteredArticles = response.data.filter(
-            (article) => article.taille === 26
+            (article) => article.taille === taille
           );
           setArticles(filteredArticles);
         } else {
@@ -42,39 +39,58 @@ export default function ArticleCard({ selectedCategorie }) {
     };
 
     fetchData();
-  }, [selectedCategorie, taille]); // Mettre à jour le tableau de dépendances pour inclure selectedCategorie et taille
+  }, [selectedCategorie]);
+
+  useEffect(() => {
+    if (selectedCategorie === 1) {
+      const filteredArticles = allArticles.filter(
+        (article) => article.taille === taille
+      );
+      setArticles(filteredArticles);
+    }
+  }, [taille, selectedCategorie, allArticles]);
 
   return (
     <>
-      <div className="pl-12">
-        <Dropdown>
-          <DropdownTrigger>
-            <Button
-              variant="bordered"
-              className="bg-black text-white text-xl h-[50px]"
+      {selectedCategorie === 1 && (
+        <div className="pl-12">
+          <Dropdown>
+            <DropdownTrigger>
+              <Button
+                variant="bordered"
+                className="bg-black text-white text-xl h-[50px]"
+              >
+                Taille Pizza
+                <IoMdArrowDropdownCircle className="ml-2" />
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+              aria-label="Static Actions"
+              className="bg-black text-white rounded-lg text-center"
             >
-              Taille Pizza
-              <IoMdArrowDropdownCircle />
-            </Button>
-          </DropdownTrigger>
-          <DropdownMenu
-            aria-label="Static Actions"
-            className="bg-black text-white rounded-lg p-4 text-center"
-          >
-            <DropdownItem key="26cm" className="hover:bg-gray-700">
-              <span className="text-xl">26 cm</span>
-            </DropdownItem>
-            <DropdownItem key="31cm" className="hover:bg-gray-700">
-              <span className="text-xl">31 cm</span>
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </div>
+              <DropdownItem
+                key="26cm"
+                className="hover:bg-gray-700"
+                onClick={() => setTaille(26)}
+              >
+                <span className="text-xl">26 cm</span>
+              </DropdownItem>
+              <DropdownItem
+                key="31cm"
+                className="hover:bg-gray-700"
+                onClick={() => setTaille(31)}
+              >
+                <span className="text-xl">31 cm</span>
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </div>
+      )}
       <div className="flex flex-wrap gap-10 pl-12 pt-8 pb-8">
         {articles.map((item, index) => (
           <div
             key={index}
-            className="bg-white text-black p-4 w-[300px] h-[515px] shadow-lg border-opacity-20 border-black border rounded-md text-center"
+            className="bg-white text-black p-4 w-[300px] h-[535px] shadow-lg border-opacity-20 border-black border rounded-md text-center"
           >
             <div className="flex justify-center items-center h-[150px]">
               <Image
@@ -85,12 +101,12 @@ export default function ArticleCard({ selectedCategorie }) {
                 className="object-cover mt-6"
               />
             </div>
-            <p className="text-2xl font-semibold mt-10">{item.libelle}</p>
+            <p className="text-2xl font-semibold mt-10">{item.articleName}</p>
             <p className="text-sm mt-3 text-gray-500 italic h-[60px]">
               {item.description}
             </p>
             <p className="text-sm mt-3">
-              Taille de l'article : {item.taille} cm
+              Taille de l'article : {item.taille} {item.tailleUnite}
             </p>
             <p className="text-2xl text-red-500 mt-3">{item.prixTtc} €</p>
             <Button
