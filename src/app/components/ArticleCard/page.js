579c6722c12e0ffe0a36cb2ebd3e013c59aa47b1
benoit <benoit.zenone@gmail.com>
2024-07-03 11:36:18 +0200

    add dropdown with choice and css

--- src/app/components/ArticleCard/page.js
+++ src/app/components/ArticleCard/page.js
@@ -2,7 +2,15 @@
 import React, { useEffect, useState } from "react";
 import { CiPizza } from "react-icons/ci";
 import { BsCart4 } from "react-icons/bs";
+import { IoMdArrowDropdownCircle } from "react-icons/io";
 import { Button } from "@nextui-org/button";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem,
+} from "@nextui-org/dropdown";
 import pizzaApi from "../../api/pizzaApi";
 import Image from "next/image";
 
@@ -37,43 +45,71 @@ export default function ArticleCard({ selectedCategorie }) {
   }, [selectedCategorie, taille]); // Mettre à jour le tableau de dépendances pour inclure selectedCategorie et taille
 
   return (
-    <div className="flex flex-wrap gap-10 pl-12 pt-8 pb-8">
-      {articles.map((item, index) => (
-        <div
-          key={index}
-          className="bg-white text-black p-4 w-[300px] h-[515px] shadow-lg border-opacity-20 border-black border rounded-md text-center"
-        >
-          <div className="flex justify-center items-center h-[150px]">
-            <Image
-              src={`/images/${item.image}`}
-              alt="Image de l'article"
-              width={180}
-              height={180}
-              className="object-cover mt-6"
-            />
+    <>
+      <div className="pl-12">
+        <Dropdown>
+          <DropdownTrigger>
+            <Button
+              variant="bordered"
+              className="bg-black text-white text-xl h-[50px]"
+            >
+              Taille Pizza
+              <IoMdArrowDropdownCircle />
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu
+            aria-label="Static Actions"
+            className="bg-black text-white rounded-lg p-4 text-center"
+          >
+            <DropdownItem key="26cm" className="hover:bg-gray-700">
+              <span className="text-xl">26 cm</span>
+            </DropdownItem>
+            <DropdownItem key="31cm" className="hover:bg-gray-700">
+              <span className="text-xl">31 cm</span>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+      <div className="flex flex-wrap gap-10 pl-12 pt-8 pb-8">
+        {articles.map((item, index) => (
+          <div
+            key={index}
+            className="bg-white text-black p-4 w-[300px] h-[515px] shadow-lg border-opacity-20 border-black border rounded-md text-center"
+          >
+            <div className="flex justify-center items-center h-[150px]">
+              <Image
+                src={`/images/${item.image}`}
+                alt="Image de l'article"
+                width={180}
+                height={180}
+                className="object-cover mt-6"
+              />
+            </div>
+            <p className="text-2xl font-semibold mt-10">{item.libelle}</p>
+            <p className="text-sm mt-3 text-gray-500 italic h-[60px]">
+              {item.description}
+            </p>
+            <p className="text-sm mt-3">
+              Taille de l'article : {item.taille} cm
+            </p>
+            <p className="text-2xl text-red-500 mt-3">{item.prixTtc} €</p>
+            <Button
+              radius="full"
+              className="bg-blue-600 text-white mt-3 hover:bg-blue-700 h-[50px] w-[220px] flex items-center justify-center space-x-2 mx-auto text-lg"
+            >
+              <CiPizza />
+              <span>Personnaliser</span>
+            </Button>
+            <Button
+              radius="full"
+              className="bg-green-600 text-white mt-3 hover:bg-green-700 h-[50px] w-[220px] flex items-center justify-center space-x-2 mx-auto text-lg"
+            >
+              <BsCart4 />
+              <span>Ajouter au panier</span>
+            </Button>
           </div>
-          <p className="text-2xl font-semibold mt-10">{item.libelle}</p>
-          <p className="text-sm mt-3 text-gray-500 italic h-[60px]">
-            {item.description}
-          </p>
-          <p className="text-sm mt-3">Taille de l'article : {item.taille} cm</p>
-          <p className="text-2xl text-red-500 mt-3">{item.prixTtc} €</p>
-          <Button
-            radius="full"
-            className="bg-blue-600 text-white mt-3 hover:bg-blue-700 h-[50px] w-[220px] flex items-center justify-center space-x-2 mx-auto text-lg"
-          >
-            <CiPizza />
-            <span>Personnaliser</span>
-          </Button>
-          <Button
-            radius="full"
-            className="bg-green-600 text-white mt-3 hover:bg-green-700 h-[50px] w-[220px] flex items-center justify-center space-x-2 mx-auto text-lg"
-          >
-            <BsCart4 />
-            <span>Ajouter au panier</span>
-          </Button>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
