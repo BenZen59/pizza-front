1760518b328db2b07b25a695c9a59ec3ea90eaae
BenZen59 <benoit.zenone@gmail.com>
2024-07-29 16:11:16 +0200

    add creation commander

--- src/app/components/CartSidebar/page.js
+++ src/app/components/CartSidebar/page.js
@@ -1,12 +1,12 @@
-import Link from "next/link";
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { removeFromCart, updateQuantity } from "@/app/redux/slices/cartSlice";
-import { FaMinus, FaPlus } from "react-icons/fa";
-import { MdOutlineDeleteOutline } from "react-icons/md";
-import { useRouter } from "next/navigation";
-import MessageConfirmationCommande from "../MessageConfirmationCommande/page";
-import Image from "next/image";
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeFromCart, updateQuantity } from '@/app/redux/slices/cartSlice';
+import { FaMinus, FaPlus } from 'react-icons/fa';
+import { MdOutlineDeleteOutline } from 'react-icons/md';
+import { useRouter } from 'next/navigation';
+import MessageConfirmationCommande from '../MessageConfirmationCommande/page';
+import Image from 'next/image';
 
 export default function CartSidebar() {
   const [showConfirmation, setShowConfirmation] = useState(false);
@@ -17,7 +17,7 @@ export default function CartSidebar() {
 
   useEffect(() => {
     // Vérifier si l'utilisateur est connecté
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
     if (token) {
       setIsLoggedIn(true);
     }
@@ -48,7 +48,7 @@ export default function CartSidebar() {
     if (isLoggedIn) {
       setShowConfirmation(true);
     } else {
-      router.push("/LoginPage"); // Rediriger vers la page de connexion
+      router.push('/LoginPage'); // Rediriger vers la page de connexion
     }
   };
 
@@ -57,64 +57,67 @@ export default function CartSidebar() {
   };
 
   return (
-    <div className="fixed right-0 top-0 w-52 h-screen shadow-lg border border-opacity-20 border-black flex flex-col pt-10 z-10">
+    <div className='fixed right-0 top-0 w-52 h-screen shadow-lg border border-opacity-20 border-black flex flex-col pt-10 z-10'>
       {loading ? (
-        <div className="py-5 px-2 text-black">Chargement ...</div>
+        <div className='py-5 px-2 text-black'>Chargement ...</div>
       ) : cartItems.length === 0 ? (
-        <div className="py-5 px-2 text-black text-center">
+        <div className='py-5 px-2 text-black text-center'>
           Le panier est vide
         </div>
       ) : (
         <>
-          <div className="p-2 flex flex-col items-center border-b border-b-black pb-4">
-            <div className="text-black">Total :</div>
-            <div className="font-bold text-red-700">{totalPrice} €</div>
+          <div className='p-2 flex flex-col items-center border-b border-b-black pb-4'>
+            <div className='text-black'>Total :</div>
+            <div className='font-bold text-red-700'>{totalPrice} €</div>
 
             <button
-              className="rounded-full bg-red-500 text-white mt-3 hover:bg-red-600 px-2 py-1 flex items-center justify-center space-x-2 mx-auto text-base"
+              className='rounded-full bg-red-500 text-white mt-3 hover:bg-red-600 px-2 py-1 flex items-center justify-center space-x-2 mx-auto text-base'
               onClick={handleShowConfirmation}
             >
               Commander
             </button>
 
             {showConfirmation && (
-              <MessageConfirmationCommande onClose={handleHideConfirmation} />
+              <MessageConfirmationCommande
+                onClose={handleHideConfirmation}
+                cartItems={cartItems}
+              />
             )}
           </div>
-          <div className="flex-1 overflow-y-auto">
+          <div className='flex-1 overflow-y-auto'>
             {cartItems.map((item) => (
               <div
                 key={`${item.idArticle}-${item.taille}-${JSON.stringify(
                   item.compositions
                 )}`}
-                className="p-4 flex flex-col items-center border-b border-b-gray-600"
+                className='p-4 flex flex-col items-center border-b border-b-gray-600'
               >
                 <Link
                   href={`product/${item.idArticle}`}
-                  className="flex items-center"
+                  className='flex items-center'
                 >
                   <Image
                     src={`/images/${item.image}`}
                     alt={item.articleName}
                     width={50}
                     height={50}
-                    className="p-1 shadow-lg border-opacity-20 border-black border"
+                    className='p-1 shadow-lg border-opacity-20 border-black border'
                   />
                 </Link>
-                <div className="text-black font-bold text-center">
+                <div className='text-black font-bold text-center'>
                   {item.articleName}
                 </div>
-                <div className="text-black text-center">
+                <div className='text-black text-center'>
                   {item.taille} {item.tailleUnite}
                 </div>
-                <div className="text-red-500 font-bold text-center">
+                <div className='text-red-500 font-bold text-center'>
                   {item.prixTtc} €
                 </div>
 
                 {/* Affichage des compositions */}
                 {item.compositions && item.compositions.length > 0 && (
-                  <div className=" text-black mt-2 text-center">
-                    <div className="font-semibold">Compositions</div>
+                  <div className=' text-black mt-2 text-center'>
+                    <div className='font-semibold'>Compositions</div>
                     <div>
                       {item.compositions.map((comp, index) => (
                         <p key={index}>
@@ -125,24 +128,24 @@ export default function CartSidebar() {
                   </div>
                 )}
 
-                <div className="flex">
-                  <div className="mt-2 flex justify-around items-center text-black space-x-2">
+                <div className='flex'>
+                  <div className='mt-2 flex justify-around items-center text-black space-x-2'>
                     <button
-                      className="bg-black text-white rounded px-2 py-1 hover:bg-gray-800 h-8 w-8 flex items-center justify-center"
+                      className='bg-black text-white rounded px-2 py-1 hover:bg-gray-800 h-8 w-8 flex items-center justify-center'
                       onClick={() => updateQuantityHandler(item, item.qty - 1)}
                     >
                       <FaMinus />
                     </button>
                     <input
-                      type="text"
+                      type='text'
                       value={item.qty}
                       onChange={(e) =>
                         updateQuantityHandler(item, parseInt(e.target.value))
                       }
-                      className="w-12 h-8 text-center border border-gray-500 rounded-md"
+                      className='w-12 h-8 text-center border border-gray-500 rounded-md'
                     />
                     <button
-                      className="bg-black text-white rounded px-2 py-1 hover:bg-gray-800 h-8 w-8 flex items-center justify-center"
+                      className='bg-black text-white rounded px-2 py-1 hover:bg-gray-800 h-8 w-8 flex items-center justify-center'
                       onClick={() => updateQuantityHandler(item, item.qty + 1)}
                     >
                       <FaPlus />
@@ -150,7 +153,7 @@ export default function CartSidebar() {
                   </div>
 
                   <button
-                    className="bg-red-500 text-white rounded-md px-2 py-1 h-8 w-8 hover:bg-red-600 mt-2 ml-4"
+                    className='bg-red-500 text-white rounded-md px-2 py-1 h-8 w-8 hover:bg-red-600 mt-2 ml-4'
                     onClick={() => removeFromCartHandler(item)}
                   >
                     <MdOutlineDeleteOutline />
--- src/app/components/MessageConfirmationCommande/page.js
+++ src/app/components/MessageConfirmationCommande/page.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
-import pizzaApi from "@/app/api/pizzaApi";
+import React, { useEffect, useState } from 'react';
+import pizzaApi from '@/app/api/pizzaApi';
 
-export default function MessageConfirmationCommande({ onClose }) {
+export default function MessageConfirmationCommande({ onClose, cartItems }) {
   const [userData, setUserData] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   useEffect(() => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
     if (token) {
       setIsLoggedIn(true);
 
@@ -20,7 +20,7 @@ export default function MessageConfirmationCommande({ onClose }) {
             idClient: idClient,
           });
         } catch (error) {
-          console.error("Failed to fetch user info", error);
+          console.error('Failed to fetch user info', error);
         }
       };
 
@@ -34,17 +34,26 @@ export default function MessageConfirmationCommande({ onClose }) {
     try {
       // Envoyer les données à l'API
       const response = await pizzaApi.createCommande(userData.idClient);
-
+      const idCommande = response.data.numeroCommande;
+      console.log(idCommande);
+      for (const item of cartItems) {
+        await pizzaApi.addArticleToCommande(
+          idCommande,
+          item.idArticle,
+          item.qty,
+          item.compositions
+        );
+      }
       // Vérifiez que `response` est défini et contient les données attendues
-      alert("Commande créée avec succès", response.data);
+      alert('Commande créée avec succès', response.data);
       onClose(); // Fermez le modal ou effectuez une autre action
     } catch (error) {
       // Utilisez `console.error` pour vérifier le contenu de l'erreur
-      console.error("Erreur lors de la création de la commande", error);
+      console.error('Erreur lors de la création de la commande', error);
       // Ajustez le message d'erreur en fonction de la structure de l'erreur retournée par l'API
       setError(
         error.response?.data?.error ||
-          "Erreur lors de la création de la commande. Veuillez réessayer."
+          'Erreur lors de la création de la commande. Veuillez réessayer.'
       );
     } finally {
       setLoading(false);
@@ -52,20 +61,20 @@ export default function MessageConfirmationCommande({ onClose }) {
   };
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white rounded-md shadow-lg p-6 text-center">
+    <div className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center'>
+      <div className='bg-white rounded-md shadow-lg p-6 text-center'>
         <p>Voulez-vous valider votre panier ?</p>
-        {error && <p className="text-red-500">{error}</p>}
-        <div className="mt-4 flex justify-around">
+        {error && <p className='text-red-500'>{error}</p>}
+        <div className='mt-4 flex justify-around'>
           <button
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'
             onClick={handleConfirm}
             disabled={loading}
           >
-            {loading ? "En cours..." : "Oui"}
+            {loading ? 'En cours...' : 'Oui'}
           </button>
           <button
-            className="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600"
+            className='px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600'
             onClick={onClose}
             disabled={loading}
           >
