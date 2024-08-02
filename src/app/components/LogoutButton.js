663c7e5acd0fa480f73d21f6398f611c258f1482
BenZen59 <benoit.zenone@gmail.com>
2024-08-02 08:41:36 +0200

    fix logout cookies

--- src/app/components/LogoutButton.js
+++ src/app/components/LogoutButton.js
@@ -1,27 +1,29 @@
 // src/components/LogoutButton.js
-"use client";
-import { useRouter } from "next/navigation";
+'use client';
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
+
 const LogoutButton = () => {
   const router = useRouter();
 
   const handleLogout = (event) => {
     event.preventDefault(); // Empêche la réactualisation de la page
-    localStorage.removeItem("token");
-    router.push("/");
+    Cookies.remove('token');
+    router.push('/');
 
     // Rediriger vers la page d'accueil et recharger la page
     try {
-      router.push("/");
+      router.push('/');
       router.reload();
     } catch (error) {
       // En cas d'échec, utiliser window.location
-      window.location.href = "/";
+      window.location.href = '/';
     }
   };
   return (
     <button
       onClick={handleLogout}
-      className="text-center h-10 w-[150px] bg-red-500 text-white font-semibold py-2 rounded-md hover:bg-red-600 transition duration-200"
+      className='text-center h-10 w-[150px] bg-red-500 text-white font-semibold py-2 rounded-md hover:bg-red-600 transition duration-200'
     >
       Se déconnecter
     </button>
